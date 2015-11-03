8f162b3683295723bddba8ab1dc01b038c1d192f
tommeke9-mac <Thomas@MacBook-Pro.local>
2015-11-03 14:59:11 +0100

    scenerecognitionNYU
    works, but huge amount of RAM necessary

--- sceneRecognitionNYU.m
+++ sceneRecognitionNYU.m
@@ -7,10 +7,10 @@ addpath data matconvnet-1.0-beta16
 %Variables:
 lastFClayer = 36;
 
-Disp('loading dataset')
+disp('loading dataset')
 load('nyu_depth_v2_labeled.mat')
-Disp('dataset loaded')
-
+disp('dataset loaded')
+[height,width,channels,dbSize] = size(images);
 
 %Setup MatConvNet
 run matconvnet-1.0-beta16/matlab/vl_setupnn;
@@ -18,27 +18,57 @@ run matconvnet-1.0-beta16/matlab/vl_setupnn;
 % load the pre-trained CNN
 net = load('imagenet-vgg-verydeep-16.mat') ; %TO BE CHANGED
 
-% load and preprocess an image
+% ------------load and preprocess an image---------------------------------
+disp('Normalization')
 % im = imread('data/office.jpg') ;
-% im_ = single(im) ; % note: 0-255 range
-% im_ = imresize(im_, net.normalization.imageSize(1:2)) ;
-% im_ = im_ - net.normalization.averageImage ;
-
-
-%--------------------Train SVM---------------------
-[height,width,channels,dbsize] = size(images);
-for i = 1:dbsize
-    
+for index = 1:dbSize
+    im(:,:,:,index) = images(:,:,:,index);
+    im_temp = single(im(:,:,:,index)) ; % note: 0-255 range
+    im_temp = imresize(im_temp, net.normalization.imageSize(1:2)) ;
+    im_(:,:,:,index) = im_temp - net.normalization.averageImage ;
 end
-%--------------------------------------------------
+disp('Normalization finished')
+%--------------------------------------------------------------------------
 
 
-% run the CNN
-res = vl_simplenn(net, im_) ;
+% ---------------------------------Run CNN---------------------------------
+disp('Run CNN')
 
-% show the classification result
-scores = squeeze(gather(res(lastFClayer+1).x)) ;
-[bestScore, best] = max(scores) ;
-figure(1) ; clf ; imagesc(im) ;
-title(sprintf('%s (%d), score %.3f',...
-net.classes.description{best}, best, bestScore)) ;
+for index = 1:dbSize
+    res(:,:,:,index) = vl_simplenn(net, im_(:,:,:,index)) ;
+    fprintf('%d of %d \n',index,dbSize);
+    whos('res')
+    %disp(num2str(index),'/',num2str(dbSize))
+end
+disp('CNN finished')
+%--------------------------------------------------------------------------
+
+
+%------------------------------Train SVM-----------------------------------
+disp('Train SVM')
+uniqueScenes = unique(sceneTypes);
+[amountOfScenes,~] = size(uniqueScenes);
+for i = 1:amountOfScenes
+    thisScene = uniqueScenes(i);
+    Disp('now training: ', thisScene)
+    positives = find(strcmp(sceneTypes,thisScene));
+    negatives = find(strcmp(sceneTypes,thisScene)==0);
+    SVMLabel = zeros(dbSize,1);
+    SVMLabel(negatives) = -1;
+    SVMLabel(positives) = 1;
+    
+    [W,B,INFO] = vl_svmtrain(res(:,:,:,:),SVMLabel,0.1,'conserveMemory', true);
+end
+disp('Training finished')
+%--------------------------------------------------------------------------
+
+
+% % run the CNN
+% res = vl_simplenn(net, im_) ;
+% 
+% % show the classification result
+% scores = squeeze(gather(res(lastFClayer+1).x)) ;
+% [bestScore, best] = max(scores) ;
+% figure(1) ; clf ; imagesc(im) ;
+% title(sprintf('%s (%d), score %.3f',...
+% net.classes.description{best}, best, bestScore)) ;
